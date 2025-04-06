30f39b5caf2dfe18605822c4ebace59920889fa4
mikdaad <mikxplode@gmail.com>
2025-04-06 21:40:06 +0530

    change -21

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,7 @@ export default function DeccanLogin() {
     <div className="w-[1245px] h-[996px] left-[79px] top-[-36px] absolute opacity-10 bg-[radial-gradient(ellipse_50.00%_50.00%_at_50.00%_50.00%,_#E8AF52_0%,_rgba(252,_232,_3,_0)_100%)] blur-3xl" />
     <img className="w-[675px] h-[818px] left-[48px] top-[48px] absolute rounded-xl" src="https://placehold.co/675x818" />
     <div className="left-[763px] top-[305px] absolute justify-center text-white text-5xl font-medium font-['Blauer_Nue']">Welcome back</div>
-    <div className="left-[763px] top-[379px] absolute justify-center"><span class="text-white text-2xl font-light font-['Blauer_Nue']">Don’t have an account? </span><span class="text-[#e8af52] text-2xl font-normal font-['Blauer_Nue']">Signup</span></div>
+    <div className="left-[763px] top-[379px] absolute justify-center"><span className="text-white text-2xl font-light font-['Blauer_Nue']">Don’t have an account? </span><span className="text-[#e8af52] text-2xl font-normal font-['Blauer_Nue']">Signup</span></div>
     <div className="w-[542px] h-[70px] left-[763px] top-[432px] absolute bg-[#d9d9d9]/20 rounded-xl" />
     <div className="left-[787px] top-[453px] absolute justify-center text-white text-opacity-30 text-2xl font-light font-['Blauer_Nue']">Enter your Phone number</div>
     <div className="px-12 py-4 left-[1030px] top-[526px] absolute bg-white bg-opacity-0 rounded outline outline-1 outline-offset-[-1px] outline-white inline-flex justify-center items-center gap-2.5">
