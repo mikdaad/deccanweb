db36a40864bdd4f5cdaf4230548445d8aa4254a0
Muhammed Mikdad <mikxplode@gmail.com>
2025-04-19 14:41:05 +0530

    94% -1

--- app/components/buildercomponents/home/Banner.tsx
+++ app/components/buildercomponents/home/Banner.tsx
@@ -49,7 +49,7 @@ export function Banner() {
 
   return (
     // Responsive Grid & Margin
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 md:ml-[7%] my-5">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 md:ml-[7%] my-5 font-blauer-nue" >
       {banners.map((banner) => (
         <Link key={banner.id} href={`/shop${banner.link}`} className="block group"> {/* Added group class */}
 
